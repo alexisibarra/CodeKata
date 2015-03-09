0da32d71845b666473ebdc32281cf839153f9757
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2015-03-09 12:20:23 -0430

    First implementation of recursive

--- Kata02-KarateChop/binarySearch.rb
+++ Kata02-KarateChop/binarySearch.rb
@@ -11,7 +11,22 @@ class BinarySearch
   end
 
   def self.recursive(value, initVal, endVal, arr)
-    return -1
+    if arr.length == 1
+      if arr[initVal] == value
+        return initVal
+      else
+        return -1
+      end
+    end
+    midVal = get_middle_point(initVal, endVal)
+
+    if arr[midVal] == value
+      return midVal
+    elsif arr[midVal] < value
+      recursive(value, midVal+1, endVal, arr)
+    else arr[midVal] > value
+      recursive(value, initVal, midVal-1, arr)
+    end
   end
 
   def self.get_middle_point (initVal, endVal)
