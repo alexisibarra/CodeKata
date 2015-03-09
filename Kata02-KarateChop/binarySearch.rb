51fddae41b60d7c15c8de30d93b43627b233401b
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2015-03-09 11:31:35 -0430

    Implementando switch-case para determinar el método a usar

--- Kata02-KarateChop/binarySearch.rb
+++ Kata02-KarateChop/binarySearch.rb
@@ -1,7 +1,16 @@
-class Recursive
-  def self.chop(value, arr)
+class BinarySearch
+  def self.chop(method, value, arr)
     if arr.empty?
       return -1
     end
+
+    case method
+    when 'recursive'
+      self.recursive(value, 0, arr.length, arr)
+    end
+  end
+
+  def self.recursive(value, initVal, endValue, arr)
+    return -1
   end
 end
