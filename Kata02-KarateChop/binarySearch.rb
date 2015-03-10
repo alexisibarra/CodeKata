da322e60a3588f6baa5e4044e5cab96aa12411a1
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2015-03-10 10:38:32 -0430

    Adding iterative binary search and optimizing recursive version

--- Kata02-KarateChop/binarySearch.rb
+++ Kata02-KarateChop/binarySearch.rb
@@ -6,30 +6,53 @@ class BinarySearch
 
     case method
     when 'recursive'
-      self.recursive(value, 0, arr.length-1, arr)
+      recursive(value, 0, arr.length-1, arr)
+    when 'iterative'
+      iterative(value, arr)
+    when 'functional'
+      functional(value, arr)
     end
   end
 
   def self.recursive(value, initVal, endVal, arr)
-    if arr.length == 1
-      if arr[initVal] == value
-        return initVal
-      else
-        return -1
-      end
+    if initVal > endVal
+      return -1
     end
+
     midVal = get_middle_point(initVal, endVal)
 
-    if arr[midVal] == value
-      return midVal
-    elsif arr[midVal] < value
+    if arr[midVal] < value
       recursive(value, midVal+1, endVal, arr)
-    else arr[midVal] > value
+    elsif arr[midVal] > value
       recursive(value, initVal, midVal-1, arr)
+    else
+      midVal
     end
   end
 
+  def self.functional
+    return -1
+  end
+
+  def self.iterative(value, arr)
+    initVal = 0
+    endVal = arr.length-1
+
+    while endVal >= initVal
+      midVal = get_middle_point(initVal, endVal)
+      if arr[midVal] == value
+        return midVal
+      elsif arr[midVal] < value
+        initVal = midVal+1
+      else
+        endVal = midVal-1
+      end
+    end
+
+    return -1
+  end
+
   def self.get_middle_point (initVal, endVal)
-    return (initVal + endVal) / 2
+    midVal = initVal + ((endVal - initVal) / 2)
   end
 end
